7c0e34cf9356ecbf1a09ec8e9a3befbdb4ef6768
Darina202 <138057834+Darina202@users.noreply.github.com>
2024-06-05 17:51:28 +0300

    upd

--- src/redux/campers/campers-selectors.js
+++ src/redux/campers/campers-selectors.js
@@ -1,9 +1,7 @@
 // export const selectAllContacts = store => store.contacts;
-import { createSelector } from '@reduxjs/toolkit';
 
 export const selectAllCampers = store => store.campers;
 
-// src/redux/campers/campers-selectors.js
 export const selectCamperById = (state, camperId) => {
   return state.campers.items.find(camper => camper._id === camperId);
 };
