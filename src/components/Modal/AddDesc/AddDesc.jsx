83d5f103f10471a93196179d002a1393d4a45f12
Darina202 <138057834+Darina202@users.noreply.github.com>
2024-06-05 22:13:43 +0300

    upd lint

--- src/components/Modal/AddDesc/AddDesc.jsx
+++ src/components/Modal/AddDesc/AddDesc.jsx
@@ -1,21 +1,21 @@
 import styles from './add-desc.module.css';
-import icons from '../../../img/icons.svg';
+// import icons from '../../../img/icons.svg';
 import BookingForm from 'components/BookingForm/BookingForm';
 import Reviews from '../../Reviews/Reviews';
 
 const AddDesc = ({ camper }) => {
   const {
-    details,
-    adults,
-    children,
-    transmission,
-    form,
-    engine,
-    length,
-    width,
-    height,
-    tank,
-    consumption,
+    // details,
+    // adults,
+    // children,
+    // transmission,
+    // form,
+    // engine,
+    // length,
+    // width,
+    // height,
+    // tank,
+    // consumption,
     reviews,
   } = camper;
   console.log(camper);
