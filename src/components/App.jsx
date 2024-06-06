a80a068fe50361093df459975f0639aad88ba37d
Darina202 <138057834+Darina202@users.noreply.github.com>
2024-06-06 09:35:10 +0300

    upd lint

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,20 +4,17 @@ import HomePage from 'pages/HomePage/HomePage';
 import CatalogPage from 'pages/CatalogPage/CatalogPage';
 import CamperPage from 'pages/CamperPage/CamperPage';
 import NotFoundPage from 'pages/NotFoundPage/NotFoundPage';
-import Container from './Container/Container';
 
 export const App = () => {
   return (
     <div>
       <MainMenu />
-      {/* <Container> */}
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/catalog" element={<CatalogPage />} />
         <Route path="/favorites" element={<CamperPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
-      {/* </Container> */}
     </div>
   );
 };
--- src/redux/campers/campers-selectors.js
+++ src/redux/campers/campers-selectors.js
@@ -1,5 +1,3 @@
-// export const selectAllContacts = store => store.contacts;
-
 export const selectAllCampers = store => store.campers;
 
 export const selectCamperById = (state, camperId) => {
