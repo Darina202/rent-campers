50336ee545d49653ad5e4c67660034936a91a48f
Darina202 <138057834+Darina202@users.noreply.github.com>
2024-06-06 11:57:18 +0300

    reviews upd

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -2,16 +2,33 @@ import styles from './reviews.module.css';
 import { useSelector } from 'react-redux';
 import { selectAllCampers } from '../../redux/campers/campers-selectors';
 import BookingForm from 'components/BookingForm/BookingForm';
+import icons from '../../img/icons.svg';
 
 const Reviews = ({ reviews }) => {
   const { isLoading, error } = useSelector(selectAllCampers);
 
   const item = reviews.map(
     ({ reviewer_name, reviewer_rating, comment }, index) => {
+      const ratingIcons = [];
+      for (let i = 0; i < reviewer_rating; i++) {
+        ratingIcons.push(
+          <svg key={i} className={styles.starIcon}>
+            <use href={`${icons}#icon-star`}></use>
+          </svg>
+        );
+      }
+
       return (
         <li key={index} className={styles.item}>
-          <h3 className={styles.author}>{reviewer_name}</h3>
-          <p>{reviewer_rating}</p>
+          <div className={styles.head}>
+            <div className={styles.round}>
+              <p className={styles.letter}>{reviewer_name.slice(0, 1)}</p>
+            </div>
+            <div>
+              <h3 className={styles.author}>{reviewer_name}</h3>
+              {ratingIcons}
+            </div>
+          </div>
           <p className={styles.comment}>{comment}</p>
         </li>
       );
